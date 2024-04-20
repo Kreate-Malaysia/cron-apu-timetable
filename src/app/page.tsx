dfa593fc9ec4c78e69ff97cb0bbdab4a067990eb
Johnson Chin <johnsonchin1009@gmail.com>
2024-04-21 01:15:22 +0800

    feat: added background for navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ export default function HomePage() {
   return (
     <>
       <div>
-        Hello World
         <Navbar />
       </div>
     </>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,40 +1,13 @@
 "use client";
 
-import Image from "next/image";
 import React from "react";
-import {useEffect, useState} from "react";
 
 export default function Navbar() {
-    const [isSticky, setIsSticky] = useState(false);
-    const [isOpen, setIsOpen] = useState(false);
-    const toggleMenu = () => {
-    setIsOpen(!isOpen);
-    };
-
-    useEffect(function scrollCalc() {
-        const handleScroll = () => {
-            const twentyPercentOfPageHeight = window.innerHeight * 0.2;
-
-      if (window.scrollY > twentyPercentOfPageHeight) {
-        setIsSticky(true);
-      } else {
-        setIsSticky(false);
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
     return (
         <>
-            <header>
-                <Image />
+          <header className="w-full h-[96px] bg-[#131313]" >
 
-            </header>
+          </header>
         </>
     )
 
